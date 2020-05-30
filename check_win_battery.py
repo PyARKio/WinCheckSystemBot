1e10da932ac6c48a1e6cfbab3b35cd1e8ba623a5
PyARKio <fedoretss@gmail.com>
2020-05-30 14:05:07 +0300

    battery check for windows 7

---
+++ check_win_battery.py
@@ -0,0 +1,38 @@
+# -- coding: utf-8 --
+from __future__ import unicode_literals
+import wmi
+from time import sleep
+
+while True:
+    c = wmi.WMI()
+    t = wmi.WMI(moniker="//./root/wmi")
+
+    batts1 = c.CIM_Battery(Caption = 'Portable Battery')
+    for i, b in enumerate(batts1):
+        print('Battery %d Design Capacity: %d mWh' % (i, b.DesignCapacity or 0))
+
+    batts = t.ExecQuery('Select * from BatteryFullChargedCapacity')
+    for i, b in enumerate(batts):
+        print('Battery %d Fully Charged Capacity: %d mWh' %
+              (i, b.FullChargedCapacity))
+
+    batts = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
+    for i, b in enumerate(batts):
+        print('\nBattery %d ***************' % i)
+        print('Tag:               ' + str(b.Tag))
+        print('Name:              ' + b.InstanceName)
+
+        print('PowerOnline:       ' + str(b.PowerOnline))
+        print('Discharging:       ' + str(b.Discharging))
+        print('Charging:          ' + str(b.Charging))
+        print('Voltage:           ' + str(b.Voltage))
+        print('DischargeRate:     ' + str(b.DischargeRate))
+        print('ChargeRate:        ' + str(b.ChargeRate))
+        print('RemainingCapacity: ' + str(b.RemainingCapacity))
+        print('Active:            ' + str(b.Active))
+        print('Critical:          ' + str(b.Critical))
+
+        sleep(5)
+
+
+
